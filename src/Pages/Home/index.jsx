e0ba509c8cb06dc6acedaa9499a2930b44d38945
Mrzakirullahkhan <121274666+Mrzakirullahkhan@users.noreply.github.com>
2025-07-07 13:11:56 +0500

    console the logs

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Crousel from './Crousel.jsx';
 import WhatweOffer from './WhatweOffer.jsx';
-import WhyChoseME from './WhyChoseME.jsx';
+import WhyChoseMe from './WhyChoseMe.jsx';
 import FreeTrail from "../../Pages/AboutUs/FreeTrail";
 import Footer from "../../Components/Footer";
 import FeesPlans from '../../Pages/Pricing/FeesPlans';
@@ -13,7 +13,7 @@ const index = () => {
       <div className="px-4 md:px-10">
         <FreeTrail />
       </div>
-      <WhyChoseME />
+      <WhyChoseMe />
       <WhatweOffer />
       <FeesPlans />
 
